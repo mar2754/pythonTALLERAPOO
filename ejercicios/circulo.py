3bcf782f8baa02ae59a2724125fd57af6a137ed4
mar2754 <munozsanchezmariana0@gmail.com>
2023-02-14 13:37:10 -0500

    Implement commit

--- ejercicios/circulo.py
+++ ejercicios/circulo.py
@@ -4,12 +4,22 @@ class Circulo:
         self.radio = radio
 
     def calcular(self):
-        PI= 3.1416
-        self.area = PI*(radio*radio)
-        self.perimetro = 2*radio*PI
+        PI=3.1416
+        self.area=PI*(radio*radio)
+        self.perimetro =2*radio*PI
 
     def get_perimetro(self):
         return self.perimetro
 
     def get_area(self):
         return self.area
+
+print("Ingrese el radio del circulo:  ")
+radio=float(input())
+
+circulo = Circulo(radio)
+circulo.calcular()
+perimetro = circulo.get_perimetro()
+print(perimetro)
+area = circulo.get_area()
+print(area)
